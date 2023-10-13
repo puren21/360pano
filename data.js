ecd88062a2ad97c75534474d62ac492aba0b38fc
puren21 <147798791+puren21@users.noreply.github.com>
2023-10-13 15:37:22 +0900

    Add files via upload
    8배 확대

---
+++ data.js
@@ -0,0 +1,86 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dji_0862-01",
+      "name": "DJI_0862-01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9235389576694537,
+          "pitch": 0.44910982349155404,
+          "rotation": 0,
+          "target": "1-dji_0861-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-dji_0861-02",
+      "name": "DJI_0861-02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.4814470501074766,
+        "pitch": 1.0336942869266146,
+        "fov": 1.5567318082068278
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5324933620732928,
+          "pitch": 0.5353345290093401,
+          "rotation": 0,
+          "target": "0-dji_0862-01"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3397328014760213,
+          "pitch": 1.4872798797304032,
+          "title": "동강오피스빌",
+          "text": "<div class=\"head_item clickArea\" style=\"overflow: hidden; height: 22px; font-size: 17px; line-height: 20px; color: rgb(136, 136, 136); text-overflow: ellipsis; text-wrap: nowrap; font-family: AppleSDGothicNeo-Regular, 돋움, dotum, sans-serif; background-color: rgb(239, 247, 255);\"><span class=\"tit_name\"><a href=\"https://map.kakao.com/#none\" data-id=\"icon\" class=\"link_order clickArea\" style=\"color: rgb(31, 140, 255); font-size: 18px; line-height: 22px; vertical-align: 0px; font-family: HelveticaNeue;\">A</a><span class=\"Apple-converted-space\">&nbsp;</span><a href=\"https://map.kakao.com/#none\" data-id=\"name\" class=\"link_name\" title=\"동강오피스빌오피스텔\" style=\"color: rgb(0, 0, 0); position: relative; z-index: 10;\"><strong>동강오피스빌오피스텔</strong></a><span class=\"Apple-converted-space\">&nbsp;</span></span><span data-id=\"subcategory\" class=\"subcategory clickable\" style=\"margin-left: 4px; font-size: 13px; line-height: 16px; vertical-align: 1px; cursor: pointer;\">오피스텔</span></div><div data-id=\"rating\" class=\"rating clickArea\" style=\"overflow: hidden; margin: 4px 0px -4px; line-height: 16px; caret-color: rgb(51, 51, 51); color: rgb(51, 51, 51); font-family: AppleSDGothicNeo-Regular, 돋움, dotum, sans-serif; background-color: rgb(239, 247, 255);\"><a href=\"https://place.map.kakao.com/8275736#review\" data-id=\"review\" class=\"review\" target=\"_blank\" style=\"color: rgb(102, 102, 102); float: left;\">리뷰<span class=\"Apple-converted-space\">&nbsp;</span><span data-id=\"numberofreview\">1</span></a></div><div class=\"info_item\" style=\"margin: 10px 0px 0px; caret-color: rgb(51, 51, 51); color: rgb(51, 51, 51); font-family: AppleSDGothicNeo-Regular, 돋움, dotum, sans-serif; font-size: 12px; background-color: rgb(239, 247, 255);\"><div data-id=\"wrapAddress\" class=\"addr\" style=\"overflow: hidden; line-height: 18px; color: rgb(34, 34, 34);\"><p data-id=\"address\" title=\"광주 남구 대남대로 351\">광주 남구 대남대로 351</p><p data-id=\"otherAddr\" class=\"lot_number\" title=\"월산동 997-4\" style=\"margin-top: 2px; color: rgb(145, 145, 145);\">(지번) 월산동 997-4</p></div></div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
