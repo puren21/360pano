dd56c6f2b53add9074eb4c0cf33a59aa7349ad7b
puren21 <147798791+puren21@users.noreply.github.com>
2023-10-13 15:56:27 +0900

    Add files via upload

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-dji_0862-01",
-      "name": "DJI_0862-01",
+      "id": "0-",
+      "name": "전경",
       "levels": [
         {
           "tileSize": 256,
@@ -16,27 +16,160 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
         }
       ],
-      "faceSize": 1024,
+      "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 0.08452959007005134,
+        "pitch": 0.6632773133636114,
+        "fov": 1.5567318082068278
       },
       "linkHotspots": [
         {
-          "yaw": -2.9235389576694537,
-          "pitch": 0.44910982349155404,
+          "yaw": 0.5289574897437213,
+          "pitch": 0.94395961858039,
+          "rotation": 4.71238898038469,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.09547918340588168,
+          "pitch": 0.7177809404231397,
+          "rotation": 4.71238898038469,
+          "target": "2-1"
+        },
+        {
+          "yaw": -0.020650976515536712,
+          "pitch": 0.8639722006163719,
           "rotation": 0,
-          "target": "1-dji_0861-02"
+          "target": "3-"
+        },
+        {
+          "yaw": -0.16960865636334255,
+          "pitch": 0.7616492166629172,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -0.01965258332042552,
+          "pitch": 0.5330073083542501,
+          "rotation": 3.141592653589793,
+          "target": "6-"
+        },
+        {
+          "yaw": -0.267735093287083,
+          "pitch": 0.5842976434310785,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5123291168417659,
+          "pitch": 0.659551035235685,
+          "title": "보성군청",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.05683233219833461,
+          "pitch": 0.659746194172083,
+          "title": "보성초등학교",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.9621385055690173,
+          "pitch": 0.9076090361225138,
+          "title": "보성남초등학교",
+          "text": ""
+        },
+        {
+          "yaw": 1.5193478972410732,
+          "pitch": 0.47315084679553365,
+          "title": "보성경찰서",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.46530060958146713,
+          "pitch": 1.2706731216096223,
+          "title": "보성군보건소",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "정문",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.17794270065767392,
+        "pitch": 0.4075084062983567,
+        "fov": 1.5567318082068278
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.013936824929317027,
+          "pitch": 0.20449789351965109,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 0.42085995988102454,
+          "pitch": 0.20001383960516428,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": -0.18700666493712248,
+          "pitch": 0.532741315899738,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.2032382065285372,
+          "pitch": 0.3885700664326386,
+          "rotation": 0,
+          "target": "2-1"
+        },
+        {
+          "yaw": -0.24930088558479468,
+          "pitch": 0.3344452566224092,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -1.51134456979368,
+          "pitch": 0.5180440452404351,
+          "rotation": 0,
+          "target": "0-"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-dji_0861-02",
-      "name": "DJI_0861-02",
+      "id": "2-1",
+      "name": "본관1",
       "levels": [
         {
           "tileSize": 256,
@@ -50,37 +183,336 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
         }
       ],
-      "faceSize": 1024,
+      "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -1.4814470501074766,
-        "pitch": 1.0336942869266146,
+        "yaw": -0.038059520966488236,
+        "pitch": 0.7007020929489904,
         "fov": 1.5567318082068278
       },
       "linkHotspots": [
         {
-          "yaw": -1.5324933620732928,
-          "pitch": 0.5353345290093401,
+          "yaw": 2.8125967390320357,
+          "pitch": 0.5487412332199852,
           "rotation": 0,
-          "target": "0-dji_0862-01"
+          "target": "1-"
+        },
+        {
+          "yaw": -3.0209796035209564,
+          "pitch": 0.26759988262931955,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.009808945480354936,
+          "pitch": 0.26253944292243503,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": -0.5908255628501227,
+          "pitch": 0.2338830946520858,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -1.599219426520655,
+          "pitch": 1.0040103506256575,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.9694543845910353,
+          "pitch": 0.5005123028842746,
+          "rotation": 0,
+          "target": "4-"
         }
       ],
-      "infoHotspots": [
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "운동장",
+      "levels": [
         {
-          "yaw": 1.3397328014760213,
-          "pitch": 1.4872798797304032,
-          "title": "동강오피스빌",
-          "text": "<div class=\"head_item clickArea\" style=\"overflow: hidden; height: 22px; font-size: 17px; line-height: 20px; color: rgb(136, 136, 136); text-overflow: ellipsis; text-wrap: nowrap; font-family: AppleSDGothicNeo-Regular, 돋움, dotum, sans-serif; background-color: rgb(239, 247, 255);\"><span class=\"tit_name\"><a href=\"https://map.kakao.com/#none\" data-id=\"icon\" class=\"link_order clickArea\" style=\"color: rgb(31, 140, 255); font-size: 18px; line-height: 22px; vertical-align: 0px; font-family: HelveticaNeue;\">A</a><span class=\"Apple-converted-space\">&nbsp;</span><a href=\"https://map.kakao.com/#none\" data-id=\"name\" class=\"link_name\" title=\"동강오피스빌오피스텔\" style=\"color: rgb(0, 0, 0); position: relative; z-index: 10;\"><strong>동강오피스빌오피스텔</strong></a><span class=\"Apple-converted-space\">&nbsp;</span></span><span data-id=\"subcategory\" class=\"subcategory clickable\" style=\"margin-left: 4px; font-size: 13px; line-height: 16px; vertical-align: 1px; cursor: pointer;\">오피스텔</span></div><div data-id=\"rating\" class=\"rating clickArea\" style=\"overflow: hidden; margin: 4px 0px -4px; line-height: 16px; caret-color: rgb(51, 51, 51); color: rgb(51, 51, 51); font-family: AppleSDGothicNeo-Regular, 돋움, dotum, sans-serif; background-color: rgb(239, 247, 255);\"><a href=\"https://place.map.kakao.com/8275736#review\" data-id=\"review\" class=\"review\" target=\"_blank\" style=\"color: rgb(102, 102, 102); float: left;\">리뷰<span class=\"Apple-converted-space\">&nbsp;</span><span data-id=\"numberofreview\">1</span></a></div><div class=\"info_item\" style=\"margin: 10px 0px 0px; caret-color: rgb(51, 51, 51); color: rgb(51, 51, 51); font-family: AppleSDGothicNeo-Regular, 돋움, dotum, sans-serif; font-size: 12px; background-color: rgb(239, 247, 255);\"><div data-id=\"wrapAddress\" class=\"addr\" style=\"overflow: hidden; line-height: 18px; color: rgb(34, 34, 34);\"><p data-id=\"address\" title=\"광주 남구 대남대로 351\">광주 남구 대남대로 351</p><p data-id=\"otherAddr\" class=\"lot_number\" title=\"월산동 997-4\" style=\"margin-top: 2px; color: rgb(145, 145, 145);\">(지번) 월산동 997-4</p></div></div>"
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
         }
-      ]
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.16648642786117662,
+        "pitch": 0.90486795552685,
+        "fov": 1.5567318082068278
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6916688321548001,
+          "pitch": 0.5192339799672894,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.8591782550008276,
+          "pitch": 0.4432344311710281,
+          "rotation": 0,
+          "target": "2-1"
+        },
+        {
+          "yaw": -0.19224398350702643,
+          "pitch": 0.15629997761662473,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 0.2807365185539634,
+          "pitch": 0.11050494515440334,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": -2.8797395556149787,
+          "pitch": 0.10329022691358958,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.7378049542038916,
+          "pitch": 0.31500381888971596,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "체육관",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.5414429297030345,
+        "pitch": 0.7603510507761975,
+        "fov": 1.5567318082068278
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1103047381349302,
+          "pitch": 0.6126075981360337,
+          "rotation": 0,
+          "target": "2-1"
+        },
+        {
+          "yaw": -2.619058671875532,
+          "pitch": 0.3833325362179565,
+          "rotation": 3.141592653589793,
+          "target": "1-"
+        },
+        {
+          "yaw": -2.539267984594204,
+          "pitch": 1.0855442183429886,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.9267964128576391,
+          "pitch": 0.18534850953508197,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.6420294078434843,
+          "pitch": 0.31824555333140836,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 1.4607472397509333,
+          "pitch": 0.13466357683716978,
+          "rotation": 0,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "\u001b별관",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.43867202111221815,
+        "pitch": 0.9554076312539763,
+        "fov": 1.5567318082068278
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2573261114466385,
+          "pitch": 0.3395033577057127,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 2.588187132212764,
+          "pitch": 0.49959001330578445,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 1.8249060332141243,
+          "pitch": 0.3548900390491898,
+          "rotation": 0,
+          "target": "2-1"
+        },
+        {
+          "yaw": 2.2480499383603725,
+          "pitch": 0.19309054631024658,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.41020549491994984,
+          "pitch": 0.2988019758940048,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 2.678821566734875,
+          "pitch": 0.13204875825814177,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-",
+      "name": "후면",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.12465504698478291,
+        "pitch": 0.5317487740722449,
+        "fov": 1.5567318082068278
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.017296580206492607,
+          "pitch": 0.5625612460863909,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.385506766645916,
+          "pitch": 0.5683872365231828,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.8347282557194564,
+          "pitch": 0.7175655804070669,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -0.3283217509333678,
+          "pitch": 0.44208469946131856,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.34505614249535377,
+          "pitch": 0.6730945107120476,
+          "rotation": 0,
+          "target": "2-1"
+        },
+        {
+          "yaw": -0.05357120856329445,
+          "pitch": 0.37265402977724627,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
     }
   ],
   "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
-    "fullscreenButton": false,
-    "viewControlButtons": false
+    "fullscreenButton": true,
+    "viewControlButtons": true
   }
 };
